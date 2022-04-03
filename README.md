982b4bb56d03bdf6f3ba50ef3b8323813b7cdc19
Jakub <93404779+nowe-konto@users.noreply.github.com>
2022-04-03 22:48:19 +0200

    Add the overlay script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PlaceEuropa
 Map of Europe on r/place
+
+## How to install the overlay?
+
+1. Install Tempermonkey for your browser
+2. Restart your browser
+3. Click this link to install the *PlaceEuropa* overlay: [https://raw.githubusercontent.com/nowe-konto/PlaceEuropa/main/src/PlaceEuropa.user.js] (install the script).
---
+++ src/PlaceEuropa.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         r/PlaceEuropa overlay
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Cover the canvas with the old continent!
+// @author       Ananas (original code from other subreddits, modified)
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://raw.githubusercontent.com/nowe-konto/PlaceEuropa/main/resources/logo.png
+// @updateURL    https://github.com/mp14O4/place-overlay/raw/main/src/scripts/overlay.js
+// @downloadURL  https://github.com/mp14O4/place-overlay/raw/main/src/scripts/overlay.js
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+              (function () {
+                  const i = document.createElement("img");
+                  i.src = "https://raw.githubusercontent.com/nowe-konto/PlaceEuropa/main/resources/map.png";
+                  i.setAttribute("class", "placeeuropa_overlay");
+                  i.style = "position: absolute; left: 0; top: 0; image-rendering: pixelated; width: 2000px; height: 2000px;";
+                  console.log(i);
+                  return i;
+              })());
+    }, false);
+  
+    document.onkeydown = function(e) {
+      const overlay = document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].getElementsByClassName("placeeuropa_overlay")[0];
+      if (e.key == "h") {
+          if (overlay.style.display) {
+            overlay.style.removeProperty('display');
+          } else {
+            overlay.style.display = "none";
+          }
+      }
+    }
+}
